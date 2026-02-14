6799a67c35a07ebf06c96184fe3924fd6920e662
dieumerci47 <dieumercitsimba04@gmail.com>
2026-02-14 09:09:26 +0100

    init talwindcss and shadcn/ui

--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const Profil = () => {
     return ( <>
 
         <h1>Profil</h1>
-
+<Button className="bg-blue-500 text-white">
+    Login
+    {/* <Link to="/login">Login</Link> */}
+</Button>
     </> );
 }
  
